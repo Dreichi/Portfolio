d3107531959dab3478a7a05d0981c4272fcf3b76
Louana <Louana.jenger@gmail.com>
2020-12-26 17:33:47 +0100

    Added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProjetsComponent } from './projets/projets.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjetsComponent,
+    AboutComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
