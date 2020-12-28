e9f0a747d8d1ec4a25bda66ee8c0526c03247e69
Louana <Louana.jenger@gmail.com>
2020-12-28 10:14:40 +0100

    Optimize code, added rputerlink and home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ProjetsComponent } from './projets/projets.component';
+import { SkillsComponent } from './skills/skills.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'projets',component:ProjetsComponent},
+  {path:'skills',component:SkillsComponent},
+  {path:'about',component:AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
